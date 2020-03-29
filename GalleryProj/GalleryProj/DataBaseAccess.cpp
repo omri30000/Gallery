b4468e2293e2c613db3581cbb73c79c53534023c
Omri Zaiman <omri162004@gmail.com>
2020-03-29 16:17:31 +0300

    added DataBaseAccess class base (only h)

---
+++ GalleryProj/GalleryProj/DataBaseAccess.cpp
@@ -0,0 +1,11 @@
+#include "DataBaseAccess.h"
+
+/*
+The function will open a database and set it up with tables
+input: none
+output: true or false id the operation succeeded
+*/
+bool DataBaseAccess::open()
+{
+	return false;
+}
